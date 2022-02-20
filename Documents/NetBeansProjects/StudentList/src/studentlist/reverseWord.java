eadab6d4451c660b96efb6ce8ab2b6f5c69fedf8
higginsf <higginsf@sheridancollege.ca>
2022-02-20 15:07:16 -0500

    removing line #40

--- Documents/NetBeansProjects/StudentList/src/studentlist/reverseWord.java
+++ Documents/NetBeansProjects/StudentList/src/studentlist/reverseWord.java
@@ -37,7 +37,6 @@ public class reverseWord {
         }*/
        /* Scanner sc = new Scanner(System.in);
         System.out.println("Please enter a word");
-        System.out.println("new line");
         String word =sc.nextLine();
         char[] myLetters = new char[word.length()];
         for(int i = 0;i<myLetters.length;i++)
